fcd530980432db3bf2aba78858b9ccb60506c7e2
AndFran <15264919+AndFran@users.noreply.github.com>
2026-06-22 11:28:11 +0200

    main now uses a custom tool
    Added a custom tool / calorie table.

--- simple_agent/main.py
+++ simple_agent/main.py
@@ -2,49 +2,82 @@ import os
 import dotenv
 import asyncio
 from openai import OpenAI
-from agents import Agent, Runner, trace
+from agents import Agent, Runner, trace, function_tool
 from openai.types.responses import ResponseTextDeltaEvent
 
-dotenv.load_dotenv()
+def setup():
+    dotenv.load_dotenv()
+
+    if not os.environ.get('OPENAI_API_KEY'):
+        print("""
+            No OPENAI_API_KEY environment variable found.
+            Set in the OS or create a .env file
+        """)
+
+    r = OpenAI().responses.create(
+        model=os.environ["OPENAI_DEFAULT_MODEL"],
+        input="Say: We are good to launch!!"
+    ).output_text
+
+    print(r)
+    print("-"*100)
 
 
-if not os.environ.get('OPENAI_API_KEY'):
-    print("""
-        No OPENAI_API_KEY environment variable found.
-        Set in the OS or create a .env file
-    """)
-
-r = OpenAI().responses.create(
-    model=os.environ["OPENAI_DEFAULT_MODEL"],
-    input="Say: We are good to launch!!"
-).output_text
-
-print(r)
-print("-"*100)
-
-# an agent needs a name and instructions (a prompt)
-nutrition_agent = Agent(
-    name="Nutrition agent",
-    instructions="""
-    You are a helpful assistant giving out nutrition advice.
-    You give concise answers.
+@function_tool
+def get_food_calories_tool(food_item: str) -> str:
     """
-)
+    Get calories for a common food to help with nutrition tracking.
+
+    :param food_item: the food we want to know the calories for i.e. apple
+    :return: the calorie information for the given food or an error message
+    """
+    calorie_data = {
+        "apple": "80 calories per medium apple (182g)",
+        "banana": "105 calories per medium banana (118g)",
+        "broccoli": "25 calories per 1 cup chopped (91g)",
+        "almonds": "164 calories per 1oz (28g) or about 23 nuts",
+    }
+
+    food_key = food_item.lower()
+    if food_key in calorie_data:
+        return f"{food_item.title()}: {calorie_data[food_key]}"
+    else:
+        return f"I dont have the calories for {food_item}"
+
 
 # an agent needs a runner, which is ASYNC and takes the prompt
 
 async def run_agent():
-    with trace("Simple nutrition agent"):
-        #result = await Runner.run(nutrition_agent, "How healthy are bananas?")
+    # an agent needs a name and instructions (a prompt)
+    nutrition_agent = Agent(
+        name="Nutrition agent",
+        instructions="""
+        You are a helpful assistant giving out nutrition advice.
+        You give concise answers.
+        """,
+        tools=[get_food_calories_tool],
+    )
 
-        response_stream = Runner.run_streamed(nutrition_agent, "How healthy are bananas?")
+    with trace("Simple nutrition agent"):
+        #result = await Runner.run(nutrition_agent, "How many calories in an apple?")
+        #print(result)
+
+        # we will now use the tool 
+        response_stream = Runner.run_streamed(nutrition_agent, "How many calories in an apple?")
         async for response in response_stream.stream_events():
             if response.type == "raw_response_event" and isinstance(response.data, ResponseTextDeltaEvent):
                 print(response.data.delta, end="", flush=True)
 
-    #print(result)
 
-asyncio.run(run_agent())
+
+
+def main():
+    setup()
+    asyncio.run(run_agent())
+
+main()
+
+
 
 
 
